add3e09f2b9fa04718580b78b161f3cd0a0e1107
Yuriy Loyko <yuraua5@gmail.com>
2023-06-25 13:07:50 +0300

    Created model, added model in admin.py, for user app

--- Library_Service_API/settings.py
+++ Library_Service_API/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "user",
     "books",
 ]
 
@@ -100,6 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "user.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
