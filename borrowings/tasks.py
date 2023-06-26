d3b63bcecbf653df8e1aa5f7890883f5655951fe
mac <yuraua5@gmail.com>
2023-06-26 22:37:56 +0300

    Implemented a daily-based function for checking borrowings overdue

--- borrowings/tasks.py
+++ borrowings/tasks.py
@@ -1,4 +1,6 @@
 import asyncio
+from datetime import timedelta
+
 import telegram
 from django.utils.timezone import localtime
 
@@ -36,9 +38,29 @@ def send_telegram_notification(borrowing_id):
     loop.run_until_complete(send_telegram_message(bot_token, chat_id, message))
 
 
+@shared_task
+def send_notification_about_overdue_borrowings():
+    tomorrow = localtime() + timedelta(days=1)
+    overdue_borrowings = Borrowing.objects.filter(
+        expected_return_date__lte=tomorrow, actual_return_date__isnull=True
+    )
+    bot_token = settings.TELEGRAM_BOT_TOKEN
+    chat_id = settings.TELEGRAM_CHAT_ID
 
+    if overdue_borrowings:
+        for borrowing in overdue_borrowings:
+            borrow_date = format_datetime(borrowing.borrow_date)
+            expected_return_date = format_datetime(borrowing.expected_return_date)
 
+            message = (
+                f"Overdue borrowing:\n"
+                f"Book: {borrowing.book.title}\n"
+                f"User: {borrowing.user.email}\n"
+                f"Borrow Date: {borrow_date}\n"
+                f"Expected Return Date: {expected_return_date}"
+            )
+            asyncio.run(send_telegram_message(bot_token, chat_id, message))
 
-
-
-
+    else:
+        message = "No borrowings overdue today!"
+        asyncio.run(send_telegram_message(bot_token, chat_id, message))
