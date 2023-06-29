1f35458d3f68f62ec16ad89216e77436c3c09f70
mac <yuraua5@gmail.com>
2023-06-29 18:50:03 +0300

    Updated README.md, added newline at end of files

--- Library_Service_API/celery.py
+++ Library_Service_API/celery.py
@@ -2,18 +2,14 @@ import os
 
 from celery import Celery
 
-# Set the default Django settings module for the 'celery' program.
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Library_Service_API.settings")
 
 app = Celery("Library_Service_API")
 
-# Using a string here means the worker doesn't have to serialize
-# the configuration object to child processes.
-# - namespace='CELERY' means all celery-related configuration keys
-#   should have a `CELERY_` prefix.
+
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
-# Load task modules from all registered Django apps.
 app.autodiscover_tasks()
 
 
